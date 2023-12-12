861f30797820cdbf9af7a0190d2a3ae1c0e65669
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-12-12 21:41:07 +0800

    edited host

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+  host: 'https://cartandtellpeerserver.onrender.com/',
+  port:10000
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
